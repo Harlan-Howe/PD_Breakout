7d409922b440a3a30627b9103e4c0b4bdb2b9a54
Harlan Howe <harlan.howe@kinkaid.org>
2024-04-08 18:03:55 -0500

    Ball moves and bounces

--- Source/main.lua
+++ Source/main.lua
@@ -1,10 +1,13 @@
 import "CoreLibs/math"
 local gfx = playdate.graphics
 
+local kbottomMargin = 234
+local ktopMargin= 22
+local kleftMargin= 6
+local krightMargin = 394
 
-
+-- create bricks
 local brickTiles, err = gfx.imagetable.new("SystemAssets/Bricks")
-
 if brickTiles == nil then
     print("image not loaded.")
     print(err)
@@ -20,10 +23,38 @@ do
    end 
 end
 
-paddleSprite = gfx.sprite.new(gfx.image.new("SystemAssets/Paddle"))
+-- create paddle
+local paddleSprite = gfx.sprite.new(gfx.image.new("SystemAssets/Paddle"))
+paddleSprite:setCollideRect(0, 0, paddleSprite:getSize())
 paddleSprite:add()
 paddleSprite:moveTo(375,120)
 
+-- create ball
+local ballSprite = gfx.sprite.new(gfx.image.new("SystemAssets/Ball"))
+ballSprite:setCollideRect(0,0,ballSprite:getSize())
+ballSprite:add()
+ballSprite:moveTo(160,30)
+local ballVelocity = {50,50}
+
+function moveBall(deltaT)
+    ballSprite:moveBy(ballVelocity[1]*deltaT, ballVelocity[2]*deltaT)
+    if ballSprite.x > krightMargin then
+        ballVelocity[1] = -1*math.abs(ballVelocity[1])
+        ballSprite.x = 2 * krightMargin - ballSprite.x
+    end
+    if ballSprite.y > kbottomMargin then
+        ballVelocity[2] = -1*math.abs(ballVelocity[2])
+        ballSprite.y = 2 * kbottomMargin - ballSprite.y
+    end
+    if ballSprite.x < kleftMargin then
+        ballVelocity[1] = math.abs(ballVelocity[1])
+        ballSprite.x = 2 * kleftMargin - ballSprite.x
+    end
+    if ballSprite.y < ktopMargin then
+        ballVelocity[2] = math.abs(ballVelocity[2])
+        ballSprite.y = 2 * ktopMargin - ballSprite.y
+    end
+end
 
 function respondToRotor()
     pos = playdate.getCrankPosition()
@@ -36,6 +67,11 @@ function playdate.update()
     gfx.setColor(gfx.kColorBlack)
     gfx.fillRect(0, 0, 400, 240)
     -- theBrick:draw(100,100)
+    
+    -- playdate.timer.updateTimers()
+    local deltaTime = playdate.getElapsedTime()
+    playdate.resetElapsedTime()
+    moveBall(deltaTime)
     respondToRotor()
     gfx.sprite.update()
     playdate.drawFPS(0,0)
