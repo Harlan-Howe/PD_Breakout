035a19e125ecdc84568f77f04991052d5b7ae357
Harlan Howe <harlan.howe@kinkaid.org>
2024-04-08 14:39:41 -0500

    Bricks and paddle appearing - paddle linked to crank

--- Source/main.lua
+++ Source/main.lua
@@ -1,8 +1,42 @@
+import "CoreLibs/math"
 local gfx = playdate.graphics
 
-gfx.setColor(gfx.kColorBlack)
+
+
+local brickTiles, err = gfx.imagetable.new("SystemAssets/Bricks")
+
+if brickTiles == nil then
+    print("image not loaded.")
+    print(err)
+end
+for row = 0,6
+do
+   for col= 1,4
+   do
+       sp = gfx.sprite.new(brickTiles:getImage(col))
+       sp:moveTo(50+16*col+8,16+32*row+16 )
+       sp:add()
+       print(row,col)
+   end 
+end
+
+paddleSprite = gfx.sprite.new(gfx.image.new("SystemAssets/Paddle"))
+paddleSprite:add()
+paddleSprite:moveTo(375,120)
+
+
+function respondToRotor()
+    pos = playdate.getCrankPosition()
+    pos = math.abs(pos-180)
+    y = playdate.math.lerp(16+24,240-24, 1-pos/180.0)
+    paddleSprite:moveTo(375,y)
+end
 
 function playdate.update()
+    gfx.setColor(gfx.kColorBlack)
     gfx.fillRect(0, 0, 400, 240)
+    -- theBrick:draw(100,100)
+    respondToRotor()
+    gfx.sprite.update()
     playdate.drawFPS(0,0)
 end
